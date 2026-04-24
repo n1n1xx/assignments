e2dd5335723cb73cc8e605e3a6d6570f00c9d885
Haneen <n1n105xx@gmail.com>
2026-04-24 15:42:58 +0200

    booking fix

--- assignment4/js/booking.js
+++ assignment4/js/booking.js
@@ -1,19 +1,51 @@
 export class Booking {
-  constructor(house) {
+  constructor(house, name, date, days, extras, code) {
     this.house = house;
+    this.name = name;
+    this.date = date;
+    this.days = days;
+    this.extras = extras;
+    this.code = code;
   }
 
-  calculateTotal(days, extras, code) {
-    let total = this.house.pricePerNight * days;
+  isValidDate() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return new Date(this.date) >= today;
+  }
 
-    if (extras.breakfast) total += 100 * days;
-    if (extras.tour) total += 300;
-    if (extras.seance) total += 500;
+  validate() {
+    if (!this.name || !this.date || !this.days) {
+      throw new Error("Fyll i alla fält");
+    }
 
-    if (code === "GHOST20") {
+    if (!this.isValidDate()) {
+      throw new Error("Datumet har redan passerat");
+    }
+  }
+
+  calculateTotal() {
+    let total = this.house.pricePerNight * this.days;
+
+    if (this.extras.breakfast) total += 100 * this.days;
+    if (this.extras.tour) total += 300;
+    if (this.extras.seance) total += 500;
+
+    if (this.code === "GHOST20") {
       total *= 0.8;
     }
 
     return Math.round(total);
+  }
+
+  getConfirmation() {
+    return `
+    Bokning bekräftad!
+    Namn: ${this.name}
+    Boende: ${this.house.name}
+    Datum: ${this.date}
+    Antal dagar: ${this.days}
+    Totalt pris: ${this.calculateTotal()} kr
+    `; 
   }
 }
